dfd17a46ef9f4d9daee73facb43120db0789eb98
liangjunjie-home <792972988@qq.com>
2019-11-18 01:34:21 +0800

    完成首页列表

--- cloudfunctions/getPlayList/index.js
+++ cloudfunctions/getPlayList/index.js
@@ -3,13 +3,30 @@ const cloud = require('wx-server-sdk');
 
 cloud.init();
 
+const db = cloud.database();
+
 const rp = require('request-promise');
 const URL = 'http://musicapi.xiecheng.live/personalized';
 
 // 云函数入口函数
 exports.main = async (event, context) => {
   const playList = await rp(URL).then(res => {
-    return JSON.parse(res);
+    return JSON.parse(res).result;
   });
-  console.log(playList);
+  for (let i = 0, len = playList.length; i < len; i++) {
+    await db
+      .collection('playList')
+      .add({
+        data: {
+          ...playList[i],
+          createTime: db.serverDate()
+        }
+      })
+      .then(res => {
+        console.log('插入成功');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 };
